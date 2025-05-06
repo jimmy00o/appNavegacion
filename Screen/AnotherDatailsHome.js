4c1226cbb879970850009b92b4fbcdbd6cddb4a3
jimmy00o <150381539+jimmy00o@users.noreply.github.com>
2025-05-06 14:18:32 -0600

    Update AnotherDatailsHome.js

--- Screen/AnotherDatailsHome.js
+++ Screen/AnotherDatailsHome.js
@@ -13,7 +13,7 @@ const style = StyleSheet.create({
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
-      backgroundColor: 'pink',
+      backgroundColor: 'blue',
     },
     containerTexto: {
       fontSize: 30,
